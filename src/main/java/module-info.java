fa134f04096b11c3fc2a11eb3197bad7239bfe33
Sergio Fernandez <sfmrbb@gmail.com>
2025-11-04 09:27:06 +0100

    Actualización de la estructura

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.planify.planifyplus {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.planify.planifyplus to javafx.fxml;
-    exports com.planify.planifyplus;
-}
