fbcbdefcad31c4949b775dd6c1fd090a1289fd42
Juankamoray <imjuanky@gmail.com>
2025-11-10 18:23:02 +0100

    primera vista login

--- src/main/java/com/planify/planifyplus/controller/LoginController.java
+++ src/main/java/com/planify/planifyplus/controller/LoginController.java
@@ -1,4 +1,13 @@
 package com.planify.planifyplus.controller;
 
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class LoginController {
+
+
 }
