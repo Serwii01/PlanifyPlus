e5325a355d631cb74e50b0da927ed4873582a19c
Ivan <ivanpastor2006@gmail.com>
2025-11-11 13:11:18 +0100

    enum correcto

--- src/main/java/com/planify/planifyplus/dto/TipoActividad.java
+++ src/main/java/com/planify/planifyplus/dto/TipoActividad.java
@@ -1,12 +1,8 @@
 package com.planify.planifyplus.dto;
 
 public enum TipoActividad {
-<<<<<<< HEAD
     CULTURAL,
     DEPORTIVA,
     TALLER
+}
 
-    //hola 123
-}
-    CULTURAL, DEPORTIVA, TALLER
-}
