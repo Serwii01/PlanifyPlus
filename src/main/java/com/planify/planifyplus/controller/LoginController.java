716fcdee5af1eb0cf8e138310ab978c5bdccbff0
JuanCarlos <juankymm64@gmail.com>
2025-11-11 12:43:14 +0100

    detalles

--- src/main/java/com/planify/planifyplus/controller/LoginController.java
+++ src/main/java/com/planify/planifyplus/controller/LoginController.java
@@ -3,10 +3,12 @@ package com.planify.planifyplus.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 
+import javax.swing.text.html.ImageView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,6 +22,11 @@ public class LoginController implements Initializable {
 
     @FXML
     private Button iniciarSesionBoton;     // <-- coincide con fx:id del FXML
+
+    @FXML
+    private Hyperlink botonNoTenerCuenta;   // <-- coincide con fx:id del FXML
+    @FXML
+    private ImageView homeCasa;   // <-- coincide con fx:id del FXML
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
