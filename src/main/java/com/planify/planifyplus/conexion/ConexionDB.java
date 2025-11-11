72b3cdce103ffe97c12eef9067aaa3b1e9918ae3
Ivan <ivanpastor2006@gmail.com>
2025-11-11 11:58:05 +0100

    ConexiónDB y META-INF con persistence.xml

--- src/main/java/com/planify/planifyplus/conexion/ConexionDB.java
+++ src/main/java/com/planify/planifyplus/conexion/ConexionDB.java
@@ -1,4 +1,18 @@
 package com.planify.planifyplus.conexion;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class ConexionDB {
+    private static final EntityManagerFactory EMF =
+            Persistence.createEntityManagerFactory("planifyPU");
+
+    public static EntityManager getEntityManager() {
+        return EMF.createEntityManager();
+    }
+
+    public static void close() {
+        if (EMF != null && EMF.isOpen()) EMF.close();
+    }
 }
