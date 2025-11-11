8140aa6f48257d42f13544fd8d5245db3f3799ab
JuanCarlos <juankymm64@gmail.com>
2025-11-11 12:43:14 +0100

    detalles

--- src/main/java/com/planify/planifyplus/dto/TipoActividad.java
+++ src/main/java/com/planify/planifyplus/dto/TipoActividad.java
@@ -1,12 +1,7 @@
 package com.planify.planifyplus.dto;
 
 public enum TipoActividad {
-<<<<<<< HEAD
     CULTURAL,
     DEPORTIVA,
     TALLER
-
-    //hola 123
-}
-    CULTURAL, DEPORTIVA, TALLER
 }
