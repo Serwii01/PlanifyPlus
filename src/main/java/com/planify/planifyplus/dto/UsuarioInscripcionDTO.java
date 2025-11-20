c3c43472d4fc9c66dc38111a132aa245577ed15b
Sergio Fernandez <sfmrbb@gmail.com>
2025-11-20 10:13:37 +0100

    actualizacion

--- src/main/java/com/planify/planifyplus/dto/UsuarioInscripcionDTO.java
+++ src/main/java/com/planify/planifyplus/dto/UsuarioInscripcionDTO.java
@@ -1,9 +1,20 @@
 package com.planify.planifyplus.dto;
 import lombok.Data;
 import jakarta.persistence.*;
+import java.sql.Timestamp;
 
 @Data
 @Entity
-@Table(name = "usuarioInscripciones")
+@Table(name = "usuariosInscripciones")
 public class UsuarioInscripcionDTO {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name="inscripcionID")
+    private long inscripcion_id;
+
+    @Column(name="inscritoEn")
+    private Timestamp inscritoEn;
 }
